2920f5217528f9170488c9376ac19e44f2b83bad
tcy <tcy19@mails.tsinghua.edu.cn>
2021-01-12 10:58:05 +0800

    Update PoseDet_DLA34_coco640.py

--- PoseDet/configs/COCO/PoseDet_DLA34_coco640.py
+++ PoseDet/configs/COCO/PoseDet_DLA34_coco640.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/dataset_coco.py',
+    # '../_base_/dataset_coco.py',
     '../_base_/PoseDet_DLA34.py',
     '../_base_/train_schedule.py',
 ]
@@ -58,6 +58,29 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
+
+dataset_type = 'CocoKeypoints'
+data_root = '/mnt/data/tcy/coco/'
+data = dict(
+    samples_per_gpu=12,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/person_keypoints_train2017.json',
+        img_prefix=data_root + 'train2017/',
+        pipeline=train_pipeline
+        ),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/person_keypoints_val2017.json',
+        img_prefix=data_root + 'val2017/',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/person_keypoints_val2017_small500.json',
+        img_prefix=data_root + 'val2017/',
+        # img_prefix=data_root + 'test2017/',
+        pipeline=test_pipeline))
 
 exp_name = 'PoseDet_DLA34_coco640'
 work_dir = './output/' + exp_name
